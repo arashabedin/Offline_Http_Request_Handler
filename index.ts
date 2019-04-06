eae8e3ea537f6bdaa4bace031bb8593ceb592d73
Arash Abedin <arash.abedin@gmail.com>
2019-04-06 18:44:14 +0200

    code optimization. and request cancellation is wokring

--- index.ts
+++ index.ts
@@ -1,37 +1,57 @@
 interface IRequest {
 	name: string;
-	isCancelled: boolean;
+	date: Date;
 }
 
 class Request {
 	//field
 	public name: string;
-	public isCancelled: boolean;
+	public date: Date;
 
 	//constructor
-	constructor(name: string, isCancelled: boolean) {
+	constructor(name: string, date: Date) {
 		this.name = name;
-		this.isCancelled = isCancelled;
+		this.date = date;
 	}
 }
 let _requests: Array<IRequest> = [];
+
+let RequestQueue = {
+	request: function(name: string, url: string, body: object, header: object, stuck, success, cancelled) {
+		let newRequest = new Request(name,new Date());
+		addRequest(newRequest);
+
+		stuck('error');
+		requestStulker(name, url,"get", header, success, body, cancelled);
+	},
+
+	cancelRequest: function(param) {
+		cancelRequest(param);
+	}
+};
+export default RequestQueue;
+
+function requestStulker(name: string, url: string,method:string, header: object, success: any, body: object, cancelled: any) {
+	setTimeout(()=>{
+		if (requestExists(name)) {
+			xhrRequest(name ,url, method, header, success, body, cancelled);
+			requestStulker(name, url,method, header, success, body, cancelled);
+			console.log("tiggered")
+		}
+		else {
+			cancelled({});
+		}
+	},4000);
+}
 
 function addRequest(newRequest: IRequest) {
 	_requests.push(newRequest);
 }
 
-function cancelRequest(name: string, bool: boolean): void {
-	_requests[name] ? (_requests[name].isCancelled = bool) : void 0;
+function cancelRequest(name: string): void {
+		_requests = _requests.filter(obj => obj.name !== name);
 }
-function isRequestCancelled(name: string): boolean {
-	let cancelled = false;
-	_requests.map((i) => {
-		if (i.name === name && i.isCancelled === true) {
-			cancelled = true;
-		}
-	});
-	return cancelled;
-}
+
 
 function requestExists(name): boolean {
 	let exists = false;
@@ -43,53 +63,38 @@ function requestExists(name): boolean {
 	return exists;
 }
 
-let RequestQueue = {
-	request: function(name: string, url: string, body: object, header: object, stuck, success, cancelled) {
-		let newRequest = new Request(name, false);
-		addRequest(newRequest);
-
-		stuck('error');
-
-		setTimeout(() => {
-			if (requestExists(name) && !isRequestCancelled(name)) {
-				const xhr = new XMLHttpRequest();
-				xhr.open('get', url, true);
-				// set the authorization HTTP header
-				const header_to_array = Object.keys(header).map((i) => [ i, header[i] ]);
-				for (let i = 0; i < header_to_array.length; i++) {
-					const eachHeader = header_to_array[i][1];
-					Object.keys(eachHeader).map((j) => {
-						xhr.setRequestHeader(j, eachHeader[j]);
-					});
-				}
-				xhr.onload = function() {
-					if (this.status >= 200 && this.status < 400) {
-						// Success!
-						const data = JSON.parse(this.response) as Response;
-						console.log(data);
-						success(data);
-					} else {
-						// We reached our target server, but it returned an error
-						console.log('error');
-					}
-				};
-
-				xhr.onerror = function(err) {
-					// There was a connection error of some sort
-					console.log('connection error');
-				};
-
-				if (body != null) {
-					xhr.send(JSON.stringify(body));
-				} else xhr.send();
-			} else {
-				cancelled({});
-			}
-		}, 2000);
-	},
-
-	cancelRequest: function(param) {
-		cancelRequest(param, true);
+function xhrRequest(name:string ,url: string, method: string, header: object, success: any, body: object, cancelled: any) {
+	if (requestExists(name)) {
+	const xhr = new XMLHttpRequest();
+	xhr.open(method, url, true);
+	// set the authorization HTTP header
+	const header_to_array = Object.keys(header).map((i) => [ i, header[i] ]);
+	for (let i = 0; i < header_to_array.length; i++) {
+		const eachHeader = header_to_array[i][1];
+		Object.keys(eachHeader).map((j) => {
+			xhr.setRequestHeader(j, eachHeader[j]);
+		});
 	}
-};
-export default RequestQueue;
+	xhr.onload = function() {
+		if (this.status >= 200 && this.status < 400) {
+			// Success!
+			const data = JSON.parse(this.response) as Response;
+			console.log(data);
+			cancelRequest(name);
+			success(data);
+		} else {
+			// We reached our target server, but it returned an error
+			console.log('error');
+		}
+	};
+	xhr.onerror = function(err) {
+		// There was a connection error of some sort
+		console.log('connection error');
+	};
+	if (body != null) {
+		xhr.send(JSON.stringify(body));
+	} else xhr.send();
+}else{
+	cancelled();
+}
+}
